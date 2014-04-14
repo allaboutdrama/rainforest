3aa5728ab48b81d052474866dc2a6b8d350f758c
allaboutdrama <allaboutdramaproductions@gmail.com>
2014-04-14 17:32:13 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rainforest::Application.config.secret_key_base = 'e6da102c577679849747c78f675b0bb26926d2c9ee133a48d7137ab4595ab5417df14186e0b805b9c404de96f94cc9f81b3e8e398483bffb0abcf53308f307c7'
